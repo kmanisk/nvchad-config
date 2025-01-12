a1ec19d8483d9436a072cbeb292ab23480be4718
kmanisk <mathewmanisk699@gmail.com>
2025-01-11 20:44:33 -0800

    added

--- nvim/lua/plugins/autocom.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"smolck/command-completion.nvim",
-	cond = function()
-		return not vim.g.vscode -- Exclude this plugin in VSCode
-	end,
-}
--- nvim/lua/plugins/vimbegood.lua
+++
@@ -1,8 +0,0 @@
-return {
-	cond = function()
-		return not vim.g.vscode -- Exclude this plugin in VSCode
-	end,
-	"ThePrimeagen/vim-be-good",
-	lazy = true, -- Lazy load the plugin
-	cmd = "VimBeGood", -- Trigger with :VimBeGood command
-}
